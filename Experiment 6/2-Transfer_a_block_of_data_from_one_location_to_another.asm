725d5b12653ce26399866156169b530674ae107a
Valerian Coelho <valerian27012004@gmail.com>
2023-07-19 19:21:15 +0530

    Code Update

--- Experiment 6/2-Transfer_a_block_of_data_from_one_location_to_another.asm
+++ Experiment 6/2-Transfer_a_block_of_data_from_one_location_to_another.asm
@@ -1,111 +1,71 @@
 section .data
-    ; Define messages to display
-    msg1 db 10, "BLOCK A DATA: ", 10  ; message for block A data
-    msg1Len equ $ - msg1             ; length of msg1
-
-    msg2 db 10, "BLOCK B DATA [After Transferring] ", 10  ; message for block B data
-    msg2Len equ $-msg2              ; length of msg2
-
-    ; Define arrays
-    array db 11h, 59h, 33h, 22h, 44h   ; initial array
-    darr db 00h, 00h, 00h, 00h, 00h   ; destination array
-    newline db '', 13, 10             ; define a newline
-
+    msg1 db 'Data Block 1 : '
+    lenMsg1 equ $-msg1
+    
+    msg2 db 13, 10, 'Data Block 2 : '
+    lenMsg2 equ $-msg2
+    
+    srcArray db 1, 2, 3, 4, 5
+    destArray db 0, 0, 0, 0, 0
+    
+    space db ' '
+    lenSpace equ $-space
+    
 section .bss
-    ; Define uninitialized variables
-    counter resb 1     ; a byte counter
-    result resb 4      ; a buffer to hold the output of disp subroutine
-
+    data resb 1
+    
 %macro write 2
-    ; A macro to display messages
+    pusha
     mov eax, 4
     mov ebx, 1
     mov ecx, %1
     mov edx, %2
     int 80h
+    popa
 %endmacro
-
+    
 section .text
-    global _start 
-_start: 
-    ; Display message for block A data
-    write msg1, msg1Len
-
-    ; Initialize the counter with the number of elements in the array
-    mov byte[counter], 05
-    mov esi, array
-
-    ; Loop through the array and display each element
-    next:   
-        mov al, [esi]
-        push esi
-        call disp
-        pop esi
+    global _start
+    
+_start:
+    write msg1, lenMsg1
+    
+    mov ecx, 5
+    mov esi, srcArray
+    
+    block1:
+        mov eax, [esi]
+        add eax, '0'
+        mov [data], eax
+        write data, 1
+        write space, lenSpace
         inc esi
-        dec byte[counter]
-        jnz next
-
-    ; Copy the array to destination array
-    mov byte[counter], 05
-    mov esi, array
-    mov edi, darr
-    next2:  
-        mov al, [esi]
-        mov [edi], al
+        loop block1
+        
+    mov ecx, 5
+    mov esi, srcArray
+    mov edi, destArray
+    
+    transfer:
+        mov eax, [esi]
+        mov [edi], eax
         inc esi
         inc edi
-        dec byte[counter]
-        jnz next2
-
-    ; Display message for block B data
-    write msg2,  msg2Len
-
-    ; Display the contents of the destination array
-    mov byte[counter], 05
-    mov edi, darr
-    next3:  
-        mov al, [edi]
-        push edi
-        call disp
-        pop edi
+        loop transfer
+        
+    write msg2, lenMsg2
+    
+    mov ecx, 5
+    mov edi, srcArray
+    
+    block2:
+        mov eax, [edi]
+        add eax, '0'
+        mov [data], eax
+        write data, 1
+        write space, lenSpace
         inc edi
-        dec byte[counter]
-        jnz next3
-
-    ; Exit the program
+        loop block2
+    
     mov eax, 1
-    mov ebx, 0
     int 80h
-
-; A subroutine to display the contents of a byte in hexadecimal format
-disp:
-        ; Initialize registers
-        mov bl, al             
-        mov edi, result        
-        mov cx, 02             
-
-    again:  
-        ; Rotate the value of BL by 4 bits to get the next nibble
-        rol bl, 04             
-        mov al, bl             
-        and al, 0fh           
-
-        ; Convert the nibble to a hexadecimal character
-        cmp al, 09            
-        jg down               
-        add al, 30h          
-        jmp skip1             
-    down:   
-        add al, 37h
-
-    skip1:  
-        ; Store the hexadecimal character in the result buffer
-        mov [edi], al          
-        inc edi            
-        dec cx                
-        jnz again            
-
-        ; Display the result buffer and a newline
-        write result, 2
-        write newline, 2
-        ret
--- Experiment 8/Display_Text_Vertically_Downwards.asm
+++ Experiment 8/Display_Text_Vertically_Downwards.asm
@@ -1,3 +1,13 @@
+section .data
+    string db 'Hello World'
+    lenString equ $-string
+    
+    newline db '', 13, 10
+    lenNewline equ $-newline
+    
+section .bss 
+    char resb 1
+    
 %macro write 2
     pusha
     mov eax, 4
@@ -7,29 +17,22 @@
     int 80h
     popa
 %endmacro
-
-Section .data
-    string db 'Hello World'
-    strlen equ $ - string
-
-    newline db '', 13, 10
-    newlineLen equ $ - newline
-
-Section .bss
-    num resb 1
-
-Section .text
+    
+section .text
     global _start
-
+    
 _start:
     mov eax, string
-    mov ecx, strlen
-    up:
-        mov [num], eax
+    mov ecx, lenString
+    
+    display:
+        mov [char], eax
+        write [char], 1
+        write newline, lenNewline
         add eax, 1
-        write [num], 1
-        write newline, newlineLen
-        loop up
-    exit:
-        mov eax,1
-        int 80H
+        loop display
+    
+    mov eax, 1
+    int 80h
+    
+    
