f6b6a2f9708cecbe2ec5f97fc7761e69ab3f6c9a
Valerian Coelho <valerian27012004@gmail.com>
2023-07-20 00:56:00 +0530

    Code Update

--- Experiment 6/3-Bubble_sort.asm
+++ Experiment 6/3-Bubble_sort.asm
@@ -1,129 +1,67 @@
-%macro write 2
-
-    PUSHA
-    mov eax,4
-    mov ebx,1
-    mov ecx,%1
-    mov edx,%2
-    int 80h
-    POPA
-%endmacro
-
-%macro read 2
-    PUSHA
-    mov eax,3
-    mov ebx,0
-    mov ecx,%1
-    mov edx,%2
-    int 80h
-    POPA
-%endmacro
-
 section .data
-msg db "Enter the number of elements : "
-msglen equ $-msg
-msg2 db "Enter the Elements : ", 13, 10
-msglen2 equ $-msg2
-msg3 db "Array Elements : ", 13, 10
-msglen3 equ $-msg3
-newline db 10, 13
-space db " "
-
+    size db 5
+    array db 5, 7, 3, 9, 1
+    
+    space db ' '
+    lenSpace equ $-space
+    
 section .bss
-arr resb 10
-size resb 2
-num1 resb 2
-temp resb 2
+    value resb 1
 
 section .text
-	global _start
-_start:
-	write msg, msglen
-	read num1, 2
-	read size, 1
-	xor eax,eax
-	xor ebx,ebx
-	xor ecx,ecx
-	    
-
-	CALL convert
-	MOV [size], ebx
-	MOV ecx, [size]
-	MOV edi, arr
-	write msg2, msglen2
-again:
-	read edi, 1
-	read edx, 1
-	sub byte[edi], '0'
-	inc eax
-	inc edi
-	dec ecx
-	jnz again
-	
-	CALL bubble
-	
-	mov eax, 0
-	mov ecx, [size]
-	MOV edi, arr
-	write msg3, msglen3
-again1:
-	add byte[edi], '0'
-	write edi, 1
-	write space, 1
-	inc eax
-	inc edi
-	dec ecx
-	jnz again1
-	
-	MOV eax, 1
-	int 80h
-	
-    convert :
-        mov esi ,num1
-        mov edi ,size
-        mov cl ,02h
-        xor eax,eax
-        xor ebx,ebx
-        up : rol bl,04h
-        mov al,[esi]
-        cmp al,39h
-        jbe skipc
-        sub al,07h
-    skipc : 
-        sub al,30h
-        add bl,al
-        mov [edi],bl
-        inc esi
-        inc edi
-        loop up
-    ret
+    global _start
     
-    bubble:
-	mov ch, [size]
-	mov esi, arr
-	;INC ESI
-	mov ah, [size]
-	loop1:
-		mov cl, [size]
-		mov edi, esi
-		dec cl
-		loop2:	
-			mov bh, [edi]
-			mov dh, [edi + 1]
-			
-		
-			CMP bh, dh
-			JBE noswap
-		
-			mov [edi], dh 
-			mov [edi + 1], bh 
-			
-	noswap:		
-			inc ah
-			inc edi
-			dec cl
-			jnz loop2
-		dec ch
-		jnz loop1 
-			
-    ret
+%macro read 2
+    pusha
+    mov eax, 3
+    mov ebx, 0
+    mov ecx, %1
+    mov edx, %2
+    int 80h
+    popa
+%endmacro
+    
+%macro write 2
+    pusha
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, %1
+    mov edx, %2
+    int 80h
+    popa
+%endmacro
+
+_start:
+    mov cl, [size] ; count for loop 1
+    loop1:
+        mov dl, [size] ; count for loop 2
+        mov edi, array ; holds address of array elements
+        loop2:
+            mov al, [edi] ; ith element
+            mov bl, [edi + 1] ; (i+1)th element
+            
+            cmp al, bl
+            jbe noSwap ; if al <= bl, then no swap req
+            mov [edi], bl
+            mov [edi + 1], al
+            
+            noSwap:
+                inc edi 
+                dec dl ; dec loop 2 counter
+                jnz loop2 
+        loop loop1 ; auto decrement of loop 1 counter
+    
+    mov cl, [size]
+    mov esi, array
+    output:
+        mov al, [esi]
+        add al, '0'
+        mov [value], al
+        write value, 1
+        write space, lenSpace
+        inc esi
+        loop output
+        
+    exit:
+        mov eax, 1
+        int 80h
